62af421c96f6aebefb1a94de1cb309a79dceb8e0
Wade Pan <deadpoolmine@qq.com>
2025-04-22 11:47:26 +0800

    to(setup): fix setup

--- .acl.o.d
+++
@@ -1,3 +0,0 @@
-trace.o: /home/deadpool/WOFS/f2fs/trace.h include/linux/kconfig.h \
-  include/generated/autoconf.h include/linux/compiler_types.h \
-  include/linux/compiler_attributes.h include/linux/compiler-clang.h
--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 target=/dev/nvme0n1p1
 mountpoint=/mnt/nvme0n1p1
 
+sudo insmod f2fs.ko
+
 sudo umount $target
 sudo mkdir -p $mountpoint
 sudo mkfs.f2fs -l wtf -f $target
